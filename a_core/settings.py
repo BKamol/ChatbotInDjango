72cd9ede76f1c89a685c2a76f4169c17d573fc33
BKamol <kamoljon2004mm@gmail.com>
2024-11-07 17:41:16 +0300

    Added private chats

--- a_core/settings.py
+++ a_core/settings.py
@@ -91,9 +91,18 @@ TEMPLATES = [
 ASGI_APPLICATION = 'a_core.asgi.application'
 
 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': "channels.layers.InMemoryChannelLayer"
+#     }
+# }
+
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': "channels.layers.InMemoryChannelLayer"
+        'BACKEND': "channels_redis.core.RedisChannelLayer",
+        'CONFIG': {
+            "hosts": ['redis://127.0.0.1:6379'],
+        }
     }
 }
 
